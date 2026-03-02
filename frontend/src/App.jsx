3788cf57d9b7d77e5cf73497fbbf3aa93f5f6cf1
Caglitoss <a22050014@unisierra.edu.mx>
2026-03-02 10:21:29 -0700

    prueba
    Se corrigió un error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/')
+      .then(res => res.text())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <>
@@ -33,11 +39,4 @@ function App() {
 
 
 }
-
-  useEffect(() => {
-  fetch('http://localhost:3001/')
-    .then(res => res.text())
-    .then(data => console.log(data));
-}, []);
-
 export default App
