9d45d995d85d2d87ba2a67be2659b631f76fb049
majo <majohemo04@gmail.com>
2026-03-02 21:32:06 -0700

    Creacion de tabla anuncios

--- database/init.sql
+++ database/init.sql
@@ -12,3 +12,21 @@ CREATE DATABASE anuncios_db
 
 -- Mensaje informativo
 \echo Base de datos "anuncios_db" creada correctamente.
+
+-- Creación de tabla anuncios
+CREATE TABLE anuncios (
+    id SERIAL PRIMARY KEY,
+
+    titulo VARCHAR(150) NOT NULL,
+    descripcion TEXT NOT NULL,
+    imagen_url TEXT,
+    documento_url TEXT,
+    estado BOOLEAN DEFAULT TRUE,
+    fecha_inicio TIMESTAMP,
+    fecha_fin TIMESTAMP,
+    es_permanente BOOLEAN DEFAULT FALSE,
+    nivel_visual INT DEFAULT 1,
+    prioridad INT DEFAULT 1,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
