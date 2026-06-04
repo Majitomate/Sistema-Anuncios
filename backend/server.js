1b09eab7662da32ddd446ff11c5726638cb9420d
majo <majohemo04@gmail.com>
2026-06-04 11:07:42 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import app, { httpServer } from './app.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
 
 httpServer.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+
+}); 
